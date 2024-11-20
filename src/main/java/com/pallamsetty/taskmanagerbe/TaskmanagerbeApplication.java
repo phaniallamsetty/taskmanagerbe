d4b31ced4a4b79f4bcf4dd295b683bcb35d72ca9
Phanindra Allamsetty <phani.allamsetty@gmail.com>
2024-11-19 21:27:20 -0500

    Initial commit

---
+++ src/main/java/com/pallamsetty/taskmanagerbe/TaskmanagerbeApplication.java
@@ -0,0 +1,13 @@
+package com.pallamsetty.taskmanagerbe;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TaskmanagerbeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TaskmanagerbeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/pallamsetty/taskmanagerbe/TaskmanagerbeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pallamsetty.taskmanagerbe;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TaskmanagerbeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
